d29925e55863c338455846e1b7261c6914057c5c
Charlie Carlegrund <charlie_carlegrund@hotmail.com>
2026-05-19 14:57:03 +0200

    påbörjat frotnend för AddPlant

--- src/services/plantService.ts
+++ src/services/plantService.ts
@@ -1,17 +1,25 @@
+import { Guid } from "guid-typescript";
 import type { Plant } from "../types/plant";
 
-export const AddPlant = (name:string,) => {
+export const AddPlant = (
+  name: string,
+  type: string,
+  interval: number,
+  imageURL: string,
+) => {
+  const NewPlant: Plant = {
+    id: Guid.create(),
+    name,
+    type,
+    interval,
+    imageURL,
+    isWatered: false,
+    nextInterval: new Date(new Date().setDate(new Date().getDate() + interval)),
+  };
+};
 
-}
+export const DeletePlant = () => {};
 
-export const DeletePlant = () =>{
+export const EditPlant = () => {};
 
-}
-
-export const EditPlant = () =>{
-
-}
-
-export const ViewPlant = () => {
-    
-}
+export const ViewPlant = () => {};
